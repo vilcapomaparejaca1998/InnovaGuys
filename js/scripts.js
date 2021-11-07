edf6c4bb888f173199fac6785aae114abb9a744a
Christian André Vilcapoma Pareja <u18102626@utp.edu.pe>
2021-11-07 18:28:57 -0500

    Proyecto Hackaton

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+/*!
+* Start Bootstrap - New Age v6.0.5 (https://startbootstrap.com/theme/new-age)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-new-age/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+
+apple1 = document.getElementById("apple1")
+apple1.addEventListener("click",function(){
+    alert("Aplicación en desarrollo")
+})
+
+apple2 = document.getElementById("apple2")
+apple2.addEventListener("click",function(){
+    alert("Aplicación en desarrollo")
+})
+
